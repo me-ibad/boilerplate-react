8a0001f6cf1e65a879a273df4f528b49c5306a86
Encodersoft <56487132+me-ibad@users.noreply.github.com>
2022-04-19 01:58:46 +0500

    admin navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,25 @@ module.exports = {
         'color-black': '#343434',
       },
     },
+    screens: {
+      xs: '300px',
+      // => @media (min-width: 640px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   variants: {
     extend: {},
