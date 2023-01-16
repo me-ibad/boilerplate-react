50482e6b3329e7d7737c10cc34b2e56a7a698197
Zeeshan Yousaf <zeeshanento34@gmail.com>
2023-01-16 21:47:56 +0500

    Update ChatBox.jsx

--- src/pages/customer/AiAssistChat/ChatBox.jsx
+++ src/pages/customer/AiAssistChat/ChatBox.jsx
@@ -14,10 +14,11 @@ function ChatBox({suggestChat,messages,responseData}) {
         <div className="chatBox-item border-2 border-gray-400 w-11/12 overflow-y-auto   m-auto text-left p-2">
           
           <div className=" flex justify-end my-3">
-            <div className="userMessage-wrapper w-7/12 ">
+            <div className="userMessage-wrapper w-full ">
               {messages.map((message, index) =>
                 message.isUser ? (
-                  <ul className="w-full">
+                  <div className=" my-4 w-full  flex justify-end  items-center">
+                  <ul className="w-6/12  flex justify-end">
                     <li className=" userMessage  m-2  flex justify-end items-center">
                       <i class="far fa-user mr-2"></i>
                       <p className=" break-all bg-blue-500 text-white rounded  p-1">
@@ -26,11 +27,12 @@ function ChatBox({suggestChat,messages,responseData}) {
                       <i class="far fa-copy ml-2"></i>
                     </li>
                   </ul>
-                ) : <div className="robotMessage-wrapper my-4 w-7/12  flex  items-center">
+                  </div>
+                ) : <div className="robotMessage-wrapper my-4 w-6/12   flex  items-center">
                 <ul className="">
                   <li className="userMessage m-2 flex items-center">
                     <i class="fas fa-robot mr-1"></i>
-                    <p className="bg-gray-200  rounded p-1">
+                    <p className="bg-gray-200 break-all  rounded p-1">
                       {message.body}
                     </p>
                     <i class="far fa-copy ml-1"></i>
