db5bc2779d2a0786f971a84132a3c4729fa0e5b6
macOS <62692034+me-saeed@users.noreply.github.com>
2023-01-16 19:17:39 +0500

    changes

--- src/pages/customer/AiAssistChat/ChatSideBar.jsx
+++ src/pages/customer/AiAssistChat/ChatSideBar.jsx
@@ -12,6 +12,8 @@ function ChatSideBar() {
     setMenuOpen(!isMenuOpen);
   };
 
+  let totalChatArray = [];
+
   // DropDown Topics
 
   // Chat
@@ -22,7 +24,7 @@ function ChatSideBar() {
     setChatScreen(true);
   };
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
+  const [newMessage, setNewMessage] = useState('');
   const [chatScreen, setChatScreen] = useState(false);
 
   const onType = (e) => {
@@ -30,20 +32,28 @@ function ChatSideBar() {
     // setAllMessages((prevState) => ({ ...prevState, body: e.target.value}));
   };
   const [responseData, setResponseData] = useState(null);
-  
+
   const onClickEnter = async (event) => {
-    // event.preventDefault();
-    //
-    //     setChatScreen(true);
-    //     setNewMessage("");
+    setChatScreen(true);
     setMessages([
       ...messages,
       {
         body: newMessage,
-        time: "2:10 AM",
-         isUser: true,
+        time: '2:10 AM',
+        isUser: true,
       },
     ]);
+    setNewMessage('');
+
+    CallAPI(newMessage);
+  };
+
+  //   setChatScreen(true);
+  //   setNewMessage("");
+
+  // };
+
+  const CallAPI = async (newMessage) => {
     const url = 'https://gpt.encodersoft.co/ask';
     const body = {
       query: newMessage,
@@ -55,77 +65,61 @@ function ChatSideBar() {
     };
 
     try {
-      const { data } = await axios.post(url,body,options)
+      const { data } = await axios.post(url, body, options);
       // setResponseData(data);
-      setChatScreen(true);
-      let array=messages
-      array.push(
+
+      setNewMessage('');
+      setMessages([
+        ...messages,
+
+        {
+          body: newMessage,
+          time: '2:10 AM',
+          isUser: true,
+        },
+
         {
           body: data.data,
-          time: "2:10 AM",
-           isUser: false,
-        }
-      )
-      console.log(array)
-      setMessages(array);
-    
+          time: '2:10 AM',
+          isUser: false,
+        },
+      ]);
     } catch (error) {
       console.error(error);
     }
   };
-  console.log(responseData,"responseData==-------")
-  console.log(messages,'-----nmesss')
-  // const onClickEnter = (event) => {
-  //   event.preventDefault();
-  //   setMessages([
-  //     ...messages,
-  //     {
-  //       body: newMessage,
-  //       time: "2:10 AM",
-  //       isUser: true,
-  //     },
-  //   ]);
-  //   setChatScreen(true);
-  //   setNewMessage("");
-  
-  // };
-    
-   
-
-  const [suggestChat, setSuggChat] = useState("");
+  const [suggestChat, setSuggChat] = useState('');
   function onQuestionChange(event) {
     setSuggChat(event.target.value);
   }
 
-
-
   return (
     <>
-      <div className="HomeScreenParentWrapper  h-screen">
+      <div className='HomeScreenParentWrapper  h-screen'>
         {/* hamburger */}
-        <div className="hamBurger w-2/12 flex justify-end items-center ">
+        <div className='hamBurger w-2/12 flex justify-end items-center '>
           <button
-            className="cursor-pointer ml-2  md:hidden text-2xl"
-            type="button"
+            className='cursor-pointer ml-2  md:hidden text-2xl'
+            type='button'
             onClick={toggleMenu}
           >
-            <i className="fas fa-bars "></i>
+            <i className='fas fa-bars '></i>
           </button>
         </div>
 
-        <div className="sideBar-Content-Container flex border-green-500">
+        <div className='sideBar-Content-Container flex border-green-500'>
           <div
             className={`sideBarWrapper w-2/12 bg-gray-400 md:block ${
-              isMenuOpen ? "block" : "hidden"
+              isMenuOpen ? 'block' : 'hidden'
             }`}
           >
             {/* side bar social navItems */}
 
-            <div className="navItems-wrapper  h-fit flex justify-center items-center">
+            <div className='navItems-wrapper  h-fit flex justify-center items-center'>
               <div>
-                <h4 className="text-white text-center">Legal -Assist .AI</h4>
-                <ul className="navItems ">
-                  <li className="homeItem">Home</li>
+                <h4 className='text-white text-center'>Legal -Assist .AI</h4>
+                <ul className='navItems '>
+                  <li className='homeItem'>Home</li>
                   <li>Briefcase</li>
                   <li>Summarize</li>
                   <li>Disclaimer</li>
@@ -134,70 +128,86 @@ function ChatSideBar() {
               </div>
             </div>
             {/* Sidebar Social icons */}
-            <div className="iconsWrapper  flex items-end h-56   ">
-              <div className="social-icons w-full  text-white flex justify-evenly">
-                <i class="fab fa-linkedin"></i>
-                <i class="fab fa-twitter-square"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-telegram"></i>
-                <i class="fab fa-android"></i>
+            <div className='iconsWrapper  flex items-end h-56   '>
+              <div className='social-icons w-full  text-white flex justify-evenly'>
+                <i class='fab fa-linkedin'></i>
+                <i class='fab fa-twitter-square'></i>
+                <i class='fab fa-instagram'></i>
+                <i class='fab fa-telegram'></i>
+                <i class='fab fa-android'></i>
               </div>
             </div>
           </div>
           {/* Chat Screen */}
-          <div className="chatQuestion w-full">
-            <div className="PopquestionContainer  w-100">
-              {chatScreen||suggestChat? (
-                <ChatBox suggestChat={suggestChat} messages={messages} responseData={responseData} />
+          <div className='chatQuestion w-full'>
+            <div className='PopquestionContainer  w-100'>
+              {chatScreen || suggestChat ? (
+                <ChatBox
+                  suggestChat={suggestChat}
+                  messages={messages}
+                  responseData={responseData}
+                />
               ) : (
                 <PopularQuestion chatHandler={chatHandler} />
               )}
             </div>
 
-            <div className="input-suggest-wrapper  flex items-center">
-              <div className="chatSuggQuestWrapper  w-3/12  flex justify-center items-center">
+            <div className='input-suggest-wrapper  flex items-center'>
+              <div className='chatSuggQuestWrapper  w-3/12  flex justify-center items-center'>
                 <select
-                  name="cars"
-                  id="cars"
-                  className="dropdown-items  rounded p-3 w-full text-center"
+                  name='cars'
+                  id='cars'
+                  className='dropdown-items  rounded p-3 w-full text-center'
                   onChange={onQuestionChange}
                 >
-                  <option value="" className="text-gray-300" disabled selected>
+                  <option value='' className='text-gray-300' disabled selected>
                     Practice Area
                   </option>
-                  <option className="dropdownColor"  value=" Ask a question question about  Family Law">
+                  <option
+                    className='dropdownColor'
+                    value=' Ask a question question about  Family Law'
+                  >
                     Family Law
                   </option>
-                  <option className="dropdownColor" value=" Ask a question question about   Business Law">
+                  <option
+                    className='dropdownColor'
+                    value=' Ask a question question about   Business Law'
+                  >
                     Business Law
                   </option>
-                  <option className="dropdownColor" value=" Ask a question question about Criminal Law">
+                  <option
+                    className='dropdownColor'
+                    value=' Ask a question question about Criminal Law'
+                  >
                     Criminal Law
                   </option>
-                  <option className="dropdownColor" value="  Ask a question question about Civil Law">
+                  <option
+                    className='dropdownColor'
+                    value='  Ask a question question about Civil Law'
+                  >
                     Civil Law
                   </option>
                 </select>
               </div>
-              <div className="chatInput-wrapper ml-2 w-9/12  h-fit w-full flex  items-end  ">
-                <div className="chatInputfield-item   w-11/12 h-fit flex items-center">
-                  <div className="input-field-item flex items-center border-2 border-black rounded w-full ">
+              <div className='chatInput-wrapper ml-2 w-9/12  h-fit w-full flex  items-end  '>
+                <div className='chatInputfield-item   w-11/12 h-fit flex items-center'>
+                  <div className='input-field-item flex items-center border-2 border-black rounded w-full '>
                     <input
-                      type="text"
-                      className=" w-full py-2.5 px-2 focus:outline-none text-center"
-                      placeholder="Continue Conversation "
+                      type='text'
+                      className=' w-full py-2.5 px-2 focus:outline-none text-center'
+                      placeholder='Continue Conversation '
                       onChange={onType}
-                      // value={newMessage}
+                      value={newMessage}
                     />
-                    <span className="">
+                    <span className=''>
                       <i
-                        class="fas fa-comment-dots text-2xl mr-4"
+                        class='fas fa-comment-dots text-2xl mr-4'
                         onClick={onClickEnter}
                       ></i>
                     </span>
                   </div>
-                  <i class="fas fa-bookmark  text-2xl ml-2"></i>
-                  <i class="fas fa-file-export ml-2 text-2xl"></i>
+                  <i class='fas fa-bookmark  text-2xl ml-2'></i>
+                  <i class='fas fa-file-export ml-2 text-2xl'></i>
                 </div>
               </div>
             </div>
